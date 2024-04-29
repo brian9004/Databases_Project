e0e5b2c569050785817d3e30791c4e8436faf7b9
JackHalliwill <jackdhalliwill@gmail.com>
2024-04-28 20:48:06 -0400

    checkout button only for logged in users

--- request.php
+++ request.php
@@ -20,10 +20,12 @@ function showBookDetails($book) {
     $html .= "</div>";
     $html .= "</div>"; // end modal-body
     $html .= "<div class='modal-footer'>";
-    $html .= "<form method='POST' action=''>";
-    $html .= "<input type='hidden' name='checkoutBookId' value='{$book['bookId']}'>";    
-    $html .= "<button type='submit' class='btn btn-success'>Checkout Book</button>";
-    $html .= "</form>";
+    if (isset($_SESSION["user_logged_in"])) {
+        $html .= "<form method='POST' action=''>";
+        $html .= "<input type='hidden' name='checkoutBookId' value='{$book['bookId']}'>";    
+        $html .= "<button type='submit' class='btn btn-success'>Checkout Book</button>";
+        $html .= "</form>";
+    }
     $html .= "<form method='POST' action=''>";
     $html .= "<input type='hidden' name='deleteBookId' value='{$book['bookId']}'>";    
     $html .= "<button type='submit' class='btn btn-danger'>Delete Book</button>";
